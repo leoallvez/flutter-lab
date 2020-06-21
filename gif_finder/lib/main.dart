23de9ee3f243abd50f1739a94493d2cbd78e3e8d
Leonardo Alves <leonardo.alves@clarifysistemas.com.br>
2020-06-20 22:00:32 -0300

    start gif app, terminado

--- gif_finder/lib/main.dart
+++ gif_finder/lib/main.dart
@@ -1,20 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:giffinder/ui/home_page.dart';
 
 void main() {
   runApp(MaterialApp(
     home: HomePage(),
+    theme: ThemeData(
+      hintColor: Colors.white,
+      primaryColor: Colors.white,
+      inputDecorationTheme: InputDecorationTheme(
+        enabledBorder:
+        OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+        focusedBorder:
+        OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+        hintStyle: TextStyle(color: Colors.white),
+      )),
   ));
-}
-
-class HomePage extends StatefulWidget {
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
 }
 
