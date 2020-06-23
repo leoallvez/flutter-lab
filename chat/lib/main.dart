262029f581d14820ec598d1230468f6340929f0a
Leonardo Alves <leonardo.alves@clarifysistemas.com.br>
2020-06-23 00:41:46 -0300

    start chat: lendo dados do firebase

--- chat/lib/main.dart
+++ chat/lib/main.dart
@@ -1,16 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-void main() {
+void main() async {
   runApp(MyApp());
   //Escrevendo dados na base de dados.
+  /*
   Firestore.instance.collection("mensagens").document().setData({
       "texto": "Tudo bem? atualiza mensagem",
       "from": "João",
       "read": false
   });
+  */
   //Lendo dados na base de dados.
-  
+  //Pegando todos os dados de um document.
+  QuerySnapshot snapshot1 = await Firestore.instance.collection("mensagens").getDocuments();
+  snapshot1.documents.forEach((doc) {
+    //print(doc.documentID);
+    //print(doc.data);
+  });
+  //Pegando varios documentos
+  DocumentSnapshot snapshot2 = await Firestore.instance.collection("mensagens").document('evkZelrHvp0lJXUVFrEn').get();
+  //print(snapshot2.data);
+
+  //Pegando dados em tempo real.
+  int count = 1;
+  Firestore.instance.collection("mensagens").snapshots().listen((snapshot) {
+    print("documentos ${count++} --------------------------------------------");
+    snapshot.documents.forEach((doc) {
+      print(doc.data);
+    });
+  });
 }
 
 class MyApp extends StatelessWidget {
